b0a31663130ee9215b3b8d8eced37a1c0c50322b
Divya Shetty <divyashettyk@gmail.com>
2021-01-15 17:27:18 -0600

    printing date and profi_loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,10 +4,14 @@ import csv
 py_bank_path = os.path.join(".", "Resources", "budget_data.csv")
 
 date =[]
+profit_loss = []
 with open(py_bank_path, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
     for row in csvreader:
         # Add title
         date.append(row[0])
+        profit_loss.append(row[1])
     print(date)
+    print(profit_loss)
+    
