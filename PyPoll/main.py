327d8578f2017c5dcaf53888fbc8a20bda3cbff3
Divya Shetty <divyashettyk@gmail.com>
2021-01-17 11:34:56 -0600

    create a dictionary for candidates and their vote counts

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,8 @@ os.system("cls")
 candidates =[]
 average = 0
 total_votes = 0
+candidate_votes = {}
+vote_count = 0
 
 #Get csv file path
 '''---------------'''
@@ -25,26 +27,40 @@ with open(py_poll_path, newline='') as csvfile:
         #get unique candidates
         if row[2] not in candidates:
             candidates.append(row[2])
-        #Total number of votes per candidates
+            #calculate total votes per candidates and add them to a dictionary.
+            vote_count+=1
+            candidate_votes[row[2]] = vote_count
+        else:
+            candidate_votes[row[2]] = candidate_votes[row[2]] + 1
+
 
         
-#printing results to terminal:    
+        
+        
+
+
+
+
+print(candidates) 
+print(candidate_votes)   
+
+
+
+##printing results to terminal:    
 print("Election Results \n----------------------------")
-
 print(f"Total Votes: {total_votes}\n----------------------------")
+#
+#print(f"Winner: {}\n----------------------------")
+#
 
-print(f"Winner: {winner}\n----------------------------")
 
-
-
-
+#Set csv file path to write
+'''------------------------'''
+poll_path = os.path.join(".", "Resources", "Poll_analysis.csv")
 # Open file in "write" mode ('w') .
-with open(file_path, 'w') as text_file:
+with open(poll_path, 'w') as text_file:
 
     # write lines into the file
-    text_file.write("Financial Analysis \n----------------------------")
-    text_file.write(f"\nTotal Months: {total_months}")
-    text_file.write(f"\nTotal: ${total_Profit_Losses}")
-    text_file.write(f"\nAverage  Change: ${round(average_change,2)}")
-    text_file.write(f"\nGreatest Increase in Profits: {profit_date} (${greatest_increase})")
-    text_file.write(f"\nGreatest Decrease in Profits: {loss_date} (${greatest_decrease})")
+    text_file.write("Election Results \n----------------------------")
+    text_file.write(f"\nTotal Votes: {total_votes}\n----------------------------)
+    
